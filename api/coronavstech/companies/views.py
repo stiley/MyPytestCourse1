aaddddd6e2a38e10b8fce06b3c25580766354592
Sean Tiley <sean.tiley@gmail.com>
2021-03-21 08:29:30 -0400

    update before import chnges

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Company
+from .serializers import CompanySerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_update")
+    pagination_class = PageNumberPagination
